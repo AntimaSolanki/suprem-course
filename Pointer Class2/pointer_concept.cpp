72777865093672361f9e117748b93fe0627239bf
AntimaSolanki <antima2002jan@gmail.com>
2023-04-30 16:21:07 +0530

    ponters

---
+++ Pointer Class2/pointer_concept.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdtr1c++.h>
+using namespace std;
+int main(){
+int arr[5]={12,23,34,45}; 
+cout<<arr<<endl;
+cout<<arr[0]<<endl;
+cout<<&arr[0]<<endl;
+cout<<&arr<<endl;
+cout<<*arr<<endl;
+cout<<(*arr+1)<<endl;
+cout<<*(arr+1)<<endl;
+cout<<arr[2]<<endl;
+cout<<*(arr+1)<<endl;
+cout<<arr[3]<<endl;
+cout<<*(arr+2)<<endl;
+    return 0;
+}
---
+++ Pointer Class2/way_twrite_pointer.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int arr[5]={1,2,3,4,5};
+int i=0;
+    cout<<arr[i]<<endl;
+    cout<<i[arr]<<endl;
+    cout<<*(arr+1)<<endl;
+    cout<<*(arr+2)<<endl;
+    cout<<*(arr+3)<<endl;
+    cout<<*(arr+4)<<endl;
+    cout<<*(arr+5)<<endl;
+
+    return 0;
+}
