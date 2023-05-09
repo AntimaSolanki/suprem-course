9c5458f16eec520c08d5fac2a712ae8afdf6471f
AntimaSolanki <antima2002jan@gmail.com>
2023-05-09 21:22:56 +0530

    coin chain problem

---
+++ Recursion Class4/coin_chain_problem.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int solve(vector<int>&arr,int target){
+    //base case 
+    if(target==0){ 
+        return 0;
+    }
+    if(target<0) {
+        return INT_MAX;
+    }
+    //ek case solve kr doo bakki recursion f=dek lega 
+    int mini=INT_MAX;
+    for(int i=0;i<arr.size();i++){
+     int ans=solve(arr,target-arr[i]);
+     if(ans!=INT_MAX)
+     int mini=min(mini,ans+1);
+    }
+    return mini;
+}
+int main(){
+vector<int>arr{1,2};
+int target=5;
+cout<<solve(arr,target)<<endl;
+    return 0;
+}
